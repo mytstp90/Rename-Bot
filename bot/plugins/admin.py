c06b816d621beaca56d5e73ac24f7aabd0c1a708
My Official Account <106927029+My-Official0698@users.noreply.github.com>
2022-06-14 22:28:00 +0530

    Update admin.py

--- bot/plugins/admin.py
+++ bot/plugins/admin.py
@@ -44,7 +44,7 @@ async def status_handler(_, m: Message):
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
     await m.reply_text(
-        text=f"**__Bot Info__** \n\n"
+        text=f"<u>ʙᴏᴛ ꜱᴛᴀᴛᴜꜱ</u> \n\n"
              f"**Disk:** `{used}` of `{total}` \n"
              f"**CPU:** `{cpu_usage}%` \n"
              f"**RAM:** `{ram_usage}%`",
