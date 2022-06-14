1c8d65187e9a0feb9bc36c706581e3f6cbf60333
My Official Account <106927029+My-Official0698@users.noreply.github.com>
2022-06-14 19:09:12 +0530

    Update display.py

--- bot/core/display.py
+++ bot/core/display.py
@@ -9,11 +9,10 @@ from pyrogram.types import Message, CallbackQuery
 from pyrogram.errors import FloodWait
 
 PROGRESS = """
-⏳ **Percentage:** `{0}%`
-✅ **Done:** `{1}`
-💠 **Total:** `{2}`
-📶 **Speed:** `{3}/s`
-🕰 **ETA:** `{4}`
+**Progress:** `{0}%`
+**Done** `{1}` ***of** `{2}`
+**Speed:** `{3}/s`
+**ETA:** `{4}`
 """
 
 
