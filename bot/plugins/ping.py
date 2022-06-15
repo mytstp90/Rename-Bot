cb3f359cd482ef5e512a4915eede572e1cca633b
My Official Account <106927029+My-Official0698@users.noreply.github.com>
2022-06-15 11:16:21 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -29,7 +29,7 @@ async def settings_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Here you can setup your settings:"
+        text="Here you can setup your settings:",
         reply_markup=types.InlineKeyboardMarkup(
             [[types.InlineKeyboardButton("Show Settings", callback_data="showSettings")]]
         ),
