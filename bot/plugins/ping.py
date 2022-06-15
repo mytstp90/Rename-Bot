86c940c25fb9eaab6895fd2d7f629e891a866ab6
My Official Account <106927029+My-Official0698@users.noreply.github.com>
2022-06-15 15:18:55 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -13,9 +13,9 @@ async def ping_handler(c: Client, m: "types.Message"):
     await c.send_flooded_message(
         chat_id=m.chat.id,
         text="Hi, User!\n\n"
-             "I can rename media without downloading it!\n"
+             "Rename media without downloading it!\n"
              "Speed depends on your media DC.\n"
-             "Just send me media and reply to it with /rename command.\n\n"
+             "Send me media and reply it with /rename command.\n\n"
              "Use /settings for changing your settings",
         reply_markup=types.InlineKeyboardMarkup([[
            types.InlineKeyboardButton("Updates", url="https://t.me/Desi_iBoTs"),
