0691017022c38ca7a8c0f48b16299a7ed40836b5
My Official Account <106927029+My-Official0698@users.noreply.github.com>
2022-06-15 11:34:52 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -8,7 +8,7 @@ from bot.core.db.add import add_user_to_database
 @Client.on_message(filters.command(["start"]) & filters.private & ~filters.edited)
 async def ping_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("I don't know about you sar 🤔")
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
@@ -17,19 +17,18 @@ async def ping_handler(c: Client, m: "types.Message"):
              "Speed depends on your media DC.\n\n"
              "Just send me media and reply to it with /rename command.",
         reply_markup=types.InlineKeyboardMarkup([[
-           types.InlineKeyboardButton("Show Settings",
-                                      callback_data="showSettings")
+           types.InlineKeyboardButton("Show Settings", callback_data="showSettings")
         ]])
     )
 
 @Client.on_message(filters.command("settings") & filters.private & ~filters.edited)
 async def settings_handler(c: Client, m: "types.Message"):
     if not m.from_user:
-        return await m.reply_text("I don't know about you sar :(")
+        return await m.reply_text("I don't know about you sar 🤔")
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Here you can setup your settings:",
+        text="Here you can setup your settings....!",
         reply_markup=types.InlineKeyboardMarkup(
             [[types.InlineKeyboardButton("Show Settings", callback_data="showSettings")]]
         ),
